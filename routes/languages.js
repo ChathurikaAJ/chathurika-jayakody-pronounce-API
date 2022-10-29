15e686ea1f2efd0d18ee30a66671a313275d87d4
Chathurika <chathurika-jayakody.com>
2022-10-29 12:25:17 -0400

    Setup audio storage using multer

--- routes/languages.js
+++ routes/languages.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const fs = require('fs');
 const textToSpeech = require('../azure/text-to-speech');
 const createAudioLink = require('../utils/audio-link')
+const multer = require('multer');
 
 // POST Text sent from User
 router.post('/text',(req,res) => {
@@ -14,18 +15,34 @@ router.post('/text',(req,res) => {
 
             // Send to SDK
             textToSpeech.speech()
-
-           
-
         }
     })
     // Send status
     res.status(200).send('User text has been successfully received')
 })
-    
+
+
+
+// Audio storage setup
+const storage = multer.diskStorage({
+    destination(req,file,cb){
+        cb(null,'./audio/');
+    },
+    filename(req,file,cb){
+        const fileNameArray = file.originalname.split('.');
+        cb(null,`${'user-audio'}.${fileNameArray[fileNameArray.length -1]}`);
+    }
+})
+
+const upload = multer({storage});
+
+
+
+// POST Audio from User
+
     
   
     
     
 
-    module.exports = router;
+module.exports = router;
